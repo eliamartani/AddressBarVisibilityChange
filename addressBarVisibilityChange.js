5d302eb82504a1cb2531b9d56ae41132c94a7ec3
Eliamar <eliamartani@gmail.com>
2018-07-09 13:59:36 +0100

    Create addressBarVisibilityChange.js

---
+++ addressBarVisibilityChange.js
@@ -0,0 +1,27 @@
+class AddressBarVisibilityChange {
+  constructor (callbackFunction) {
+    this.previousHeight = window.innerHeight
+    this.callbackFunction = callbackFunction
+    this.resizeEventHandler = this.resizeEvent.bind(this)
+
+    window.addEventListener('resize', this.resizeEventHandler, false)
+  }
+
+  destroy () {
+    // remove event listener
+    window.removeEventListener('resize', this.resizeEventHandler, false)
+  }
+
+  resizeEvent () {
+    // detect height change by comparing with previous height
+    if (this.previousHeight !== window.innerHeight) {
+      const isFunction = func => func && Object.prototype.toString.call(func).toLowerCase().indexOf('function') > -1
+
+      // check if it's undefined and is a function
+      if (isFunction(this.callbackFunction)) this.callbackFunction()
+
+      // set current height before losing it
+      this.previousHeight = window.innerHeight
+    }
+  }
+}
